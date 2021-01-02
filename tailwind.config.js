588132bd9646252ced6f96ce8dbfbd7477288369
Jorge <jorgecbll@gmail.com>
2021-01-02 19:18:33 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  variants: {},
+  plugins: [],
+}
