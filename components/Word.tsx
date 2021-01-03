b85c7c00baed72803f8b7f68513ddc4f505d2c53
Jorge <jorgecbll@gmail.com>
2021-01-02 21:47:24 -0300

    fix build

---
+++ components/Word.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import words from "utils/word-generator";
+const Word = ({ isPlaying }) => {
+  const [word, setWord] = useState(words)
+
+  useEffect(() => {
+    if (!isPlaying) return;
+    const timer = setTimeout(() => {
+      setWord(words)
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [isPlaying, word])
+
+  return <div>
+    <p className="text-sm">{isPlaying ? 'Playing: ' : 'Stopped'}</p>
+    <h1 className="text-7xlxl py-4">{isPlaying ? `${word}` : ``}</h1>
+  </div>
+}
+export default Word
